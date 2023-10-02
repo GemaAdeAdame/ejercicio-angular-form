c35e7f17fc370c03979e47822f518abbc6d420e3
gemaadeadame <gema.a.p.7@gmail.com>
2023-10-02 17:27:37 +0200

    I commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ejercicio-angular-form';
+
+
+export class AppComponent{
+  
+ forma!: FormGroup;
+  constructor(private fb:FormBuilder) {
+    this.crearFormulario();
+  }
+
+  crearFormulario(){
+   this.forma = this.fb.group({
+    nombre:[''],
+    nif:[''],
+    email:[''],
+    password:['']
+
+   })
+  }
+guardar(){
+  console.log(this.forma)
 }
+limpiar(){
+  this.forma.reset();
+}
+}
+
